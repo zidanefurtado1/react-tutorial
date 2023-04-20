0aad723a317cd96cf4b89a3d4ab348573e1d3dbe
zidanefurtado1 <100311249+zidanefurtado1@users.noreply.github.com>
2023-04-20 17:00:00 +0100

    react

--- react_example_03-ComponentsReuse/like_button.js
+++ react_example_03-ComponentsReuse/like_button.js
@@ -1,1 +1,61 @@
 'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = { liked: false };
+
+    }
+
+    render() {
+
+        if (this.state.liked) {
+
+            return 'You liked this.';
+
+        }
+
+        return e('button', { onClick: () => this.setState({ liked: true }) }, 'Like');
+
+    }
+
+    render() {
+
+        if (this.state.liked) {
+
+            return 'You liked comment number ' + this.props.commentID;
+
+        }
+
+        return e('button', { onClick: () => this.setState({ liked: true }) }, 'Like');
+
+    }
+
+}
+
+const reactContainers = document.querySelectorAll(".root");
+
+// Find all DOM containers, and render Like buttons into them.
+
+document.querySelectorAll('.like_button_container')
+
+    .forEach(domContainer => {
+
+        // Read the comment ID from a data-* attribute.
+
+        const commentID = parseInt(domContainer.dataset.commentid);
+
+        ReactDOM.render(
+
+            e(LikeButton, { commentID: commentID }),
+
+            domContainer
+
+        );
+
+    });
